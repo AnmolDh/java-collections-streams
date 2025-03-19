ed1f1edbd6d1849fb14f53f0ef77db9fad65ee96
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-19 22:05:23 +0530

    feat: (generics) implemented product catalog

---
+++ 01 - Generics/DynamicOnlineMarketplace/src/Product.java
@@ -0,0 +1,31 @@
+import product_categories.*;
+
+public class Product <T extends ICategory>{
+    private String name;
+    private T category;
+
+    public Product(String name, T category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    public void setCategory(T category) {
+        this.category = category;
+    }
+
+    public static <T extends Product<?>> void applyDiscount(T product, double percentage) {
+        System.out.println("Discount applied for " + product.getName() + " with percentage " + percentage);
+    }
+}
---
+++ 01 - Generics/DynamicOnlineMarketplace/src/product_categories/BookCategory.java
@@ -0,0 +1,17 @@
+package product_categories;
+
+public class BookCategory implements ICategory {
+    private String name;
+
+    public BookCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
