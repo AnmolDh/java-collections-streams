f2d40877f702b3d57266928543c7ed17b353b9c0
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-21 15:12:52 +0530

    feat: (collections) implemented Banking System

---
+++ 02 - Collections/BankingSystem/src/com/banking/Main.java
@@ -0,0 +1,23 @@
+package com.banking;
+
+import com.banking.account.Account;
+import com.banking.bank.Bank;
+
+public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+
+        Account account = new Account("201", "Savings");
+        Account account2 = new Account("3442", "Salary");
+        Account account3 = new Account("532", "Savings");
+
+        bank.addAccount(account, 1000);
+        bank.addAccount(account2, 33100);
+        bank.addAccount(account3, 5000);
+
+        bank.requestWithdrawal(account, 500);
+        bank.requestWithdrawal(account, 600);
+
+        bank.processWithdrawalRequests();
+    }
+}
---
+++ 02 - Collections/BankingSystem/src/com/banking/account/WithdrawalRequest.java
@@ -0,0 +1,27 @@
+package com.banking.account;
+
+public class WithdrawalRequest {
+    private String accountNumber;
+    private int amount;
+
+    public WithdrawalRequest(String accountNumber, int amount) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
