667779394ffc498280d8fed11ca3dffdb843264e
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-21 10:00:03 +0530

    feat: (collections) implemented check Subset

---
+++ 02 - Collections/Set Interface/Subset.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Subset{
+  public static boolean checkSubset(HashSet<Integer> h1, HashSet<Integer> h2){
+    for (int i : h1){
+      if (!h2.contains(i)) return false;
+    }
+    return true;
+  }
+
+  public static void main(String[] args){
+    HashSet<Integer> h1 = new HashSet<>();
+    HashSet<Integer> h2 = new HashSet<>();
+
+    h1.add(2);
+    h1.add(3);
+    h2.add(2);
+    h2.add(3);
+    h2.add(5);
+
+    System.out.println(checkSubset(h1, h2));
+  }
+}
