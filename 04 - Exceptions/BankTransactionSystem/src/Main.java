c4abe8d634914ac67d6e7ecb51cf7da422cd9824
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-24 18:38:34 +0530

    feat: (exceptions) implemented Banking Transaction System

---
+++ 04 - Exceptions/BankTransactionSystem/src/Main.java
@@ -0,0 +1,16 @@
+import banking.Transaction;
+import custom_exceptions.InsufficientBalanceException;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            Transaction.withdraw(10000, 1000);
+        }
+        catch (InsufficientBalanceException e){
+            System.out.println(e.getMessage());
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ 04 - Exceptions/BankTransactionSystem/src/banking/Transaction.java
@@ -0,0 +1,15 @@
+package banking;
+
+import custom_exceptions.InsufficientBalanceException;
+
+public class Transaction {
+    public static void withdraw(double amount, double balance) throws InsufficientBalanceException {
+        if (amount > balance) {
+            throw new InsufficientBalanceException(amount, balance);
+        }
+        if (amount < 0){
+            throw new IllegalArgumentException("Invalid amount");
+        }
+        amount -= balance;
+    }
+}
