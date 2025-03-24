7293d64843393979ce19cf2de0752a1c1d564352
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-24 18:19:29 +0530

    feat: (exceptions) implemented finally Block

---
+++ 04 - Exceptions/FinallyBlock/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            System.out.println(x / y);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Division by zero");
+        }
+        finally {
+            System.out.println("Operation Completed");
+        }
+    }
+}
