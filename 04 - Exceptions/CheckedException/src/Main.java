0281590ff6569de1c3a827f544184868b1016fba
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-24 13:03:34 +0530

    feat: (exceptions) implemented Checked Exception

---
+++ 04 - Exceptions/CheckedException/src/Main.java
@@ -0,0 +1,7 @@
+import file_handler.Reader;
+
+public class Main {
+    public static void main(String[] args) {
+        Reader.read("randomPath");
+    }
+}
---
+++ 04 - Exceptions/CheckedException/src/file_handler/Reader.java
@@ -0,0 +1,19 @@
+package file_handler;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+    public static void read(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException ie){
+            System.out.println("File not found");
+        }
+    }
+}
