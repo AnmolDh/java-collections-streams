0d3386ddb07c803efe850d5a849cf16c493c358e
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-25 13:24:41 +0530

    feat: (regex) implemented Capital Word Extraction

--- 05 - Regex & JUnit/Regex/src/Main.java
+++ 05 - Regex & JUnit/Regex/src/Main.java
@@ -10,5 +10,9 @@ public class Main {
         ValidateHexColor.validate("#FFA500");
 
         ExtractEmails.extract("Contact us at support@example.com and info@company.org");
+
+        ExtractCapitalizedWords.extract("The Eiffel Tower is in Paris and the Statue of Liberty is in New York.");
+
+//        ExtractDates.extract("The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.");
     }
 }
