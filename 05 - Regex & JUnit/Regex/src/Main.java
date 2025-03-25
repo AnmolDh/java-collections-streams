1f0b2619a3631945826773236e54dd3f29742338
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-25 13:44:02 +0530

    feat: (regex) implemented Links Extraction

--- 05 - Regex & JUnit/Regex/src/Main.java
+++ 05 - Regex & JUnit/Regex/src/Main.java
@@ -3,16 +3,18 @@ import regex_validations.*;
 
 public class Main {
     public static void main(String[] args) {
-        ValidateUsername.validate("user_123");
+//        ValidateUsername.validate("user_123");
+//
+//        ValidateLicensePlate.validate("HR0110");
+//
+//        ValidateHexColor.validate("#FFA500");
+//
+//        ExtractEmails.extract("Contact us at support@example.com and info@company.org");
+//
+//        ExtractCapitalizedWords.extract("The Eiffel Tower is in Paris and the Statue of Liberty is in New York.");
+//
+//        ExtractDates.extract("The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.");
 
-        ValidateLicensePlate.validate("HR0110");
-
-        ValidateHexColor.validate("#FFA500");
-
-        ExtractEmails.extract("Contact us at support@example.com and info@company.org");
-
-        ExtractCapitalizedWords.extract("The Eiffel Tower is in Paris and the Statue of Liberty is in New York.");
-
-        ExtractDates.extract("The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.");
+        ExtractLinks.extract("Visit https://www.google.com and http://example.org for more info.");
     }
 }
